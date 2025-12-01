55b3980fc15e7aa8968159d2cbad0e6f17207edb
j0shtran <hienjoshuatran@gmail.com>
2025-12-01 16:44:12 -0600

    final

--- algo1.cpp
+++ algo1.cpp
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include <iostream>
 #include <vector>
 using namespace std;
 
 //BUGGY CODE
 bool fail_lecture(const vector<int>& attendance_records) {
   int absent_count = 0;
-  for (int i = 1; i < attendance_records.size(); ++i) {
+  for (int i = 1; i < (int)attendance_records.size(); ++i) {
     absent_count += attendance_records[i] == 0;
   }
 
@@ -15,7 +15,7 @@ bool fail_lecture(const vector<int>& attendance_records) {
 //FIXED CODE
 bool fail_lecture_fix(const vector<int>& attendance_records) {
   int absent_count = 0;
-  for (int i = 0; i < attendance_records.size(); ++i) {
+  for (int i = 0; i < (int)attendance_records.size(); ++i) {
     absent_count += attendance_records[i] == 0;
   }
 
@@ -28,4 +28,12 @@ void test(const vector<int>& arr) {
   cout << "\n";
   cout << "Correct Output: " << fail_lecture_fix(arr) << "\n";
   cout << "Incorrect Output: " <<fail_lecture(arr) << "\n";
+}
+
+int main() {
+  test({1});
+  test({1,0,0,0,1,1,1,1});
+  test({0,0,1,1,1,1});
+  test({0,1,0,0,1,1});
+  return 0;
 }
